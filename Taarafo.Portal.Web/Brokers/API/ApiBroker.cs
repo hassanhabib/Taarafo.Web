edcbebf870214c3bc0280df63b7814be56e05f1c
Hassan Habib <hassanhabib@live.com>
2021-10-20 21:37:06 -0700

    CONFIG: Localize Api Configuration

--- Taarafo.Portal.Web/Brokers/API/ApiBroker.cs
+++ Taarafo.Portal.Web/Brokers/API/ApiBroker.cs
@@ -3,17 +3,25 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using RESTFulSense.Clients;
+using Taarafo.Portal.Web.Models.Configurations;
 
 namespace Taarafo.Portal.Web.Brokers.API
 {
     public partial class ApiBroker : IApiBroker
     {
         private readonly IRESTFulApiFactoryClient apiClient;
+        private readonly HttpClient httpClient;
 
-        public ApiBroker(IRESTFulApiFactoryClient apiClient) =>
-            this.apiClient = apiClient;
+        public ApiBroker(HttpClient httpClient, IConfiguration configuration)
+        {
+            this.httpClient = httpClient;
+            this.apiClient = GetApiClient(configuration);
+        }
 
         private async ValueTask<T> GetAsync<T>(string relativeUrl) =>
             await this.apiClient.GetContentAsync<T>(relativeUrl);
@@ -26,5 +34,17 @@ namespace Taarafo.Portal.Web.Brokers.API
 
         private async ValueTask<T> DeleteAsync<T>(string relativeUrl) =>
             await this.apiClient.DeleteContentAsync<T>(relativeUrl);
+
+
+        private IRESTFulApiFactoryClient GetApiClient(IConfiguration configuration)
+        {
+            LocalConfigurations localConfigurations =
+                configuration.Get<LocalConfigurations>();
+
+            string apiBaseUrl = localConfigurations.ApiConfigurations.Url;
+            this.httpClient.BaseAddress = new Uri(apiBaseUrl);
+
+            return new RESTFulApiFactoryClient(this.httpClient);
+        }
     }
 }
--- Taarafo.Portal.Web/Startup.cs
+++ Taarafo.Portal.Web/Startup.cs
@@ -3,17 +3,13 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using RESTFulSense.Clients;
 using Taarafo.Portal.Web.Brokers.API;
 using Taarafo.Portal.Web.Brokers.Loggings;
-using Taarafo.Portal.Web.Models.Configurations;
 
 namespace Taarafo.Portal.Web
 {
@@ -28,10 +24,10 @@ namespace Taarafo.Portal.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            AddHttpClient(services);
             AddRootDirectory(services);
+            services.AddLogging();
+            services.AddHttpClient();
             services.AddScoped<IApiBroker, ApiBroker>();
-            services.AddScoped<ILogger, Logger<LoggingBroker>>();
             services.AddScoped<ILoggingBroker, LoggingBroker>();
         }
 
@@ -55,16 +51,6 @@ namespace Taarafo.Portal.Web
             {
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
-            });
-        }
-
-        private void AddHttpClient(IServiceCollection services)
-        {
-            services.AddHttpClient<IRESTFulApiFactoryClient, RESTFulApiFactoryClient>(client =>
-            {
-                LocalConfigurations localConfigurations = Configuration.Get<LocalConfigurations>();
-                string apiUrl = localConfigurations.ApiConfigurations.Url;
-                client.BaseAddress = new Uri(apiUrl);
             });
         }
 
