23b7343e9fe5dcf9139b63ba557fe425bb460c7b
Ricky <rcruzfreelance@outlook.com>
2021-11-04 22:52:00 -0400

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> PASS

---
+++ Taarafo.Portal.Web/Services/Foundations/Posts/PostService.Expections.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Taarafo.Portal.Web.Models.Posts;
+using Taarafo.Portal.Web.Models.Posts.Exceptions;
+
+namespace Taarafo.Portal.Web.Services.Foundations.Posts
+{
+    public partial class PostService
+    {
+        private delegate ValueTask<Post> ReturningPostFunction();
+
+        private async ValueTask<Post> TryCatch(ReturningPostFunction returningPostFunction)
+        {
+            try
+            {
+                return await returningPostFunction();
+            }
+            catch (InvalidPostException invalidPostException)
+            {
+                throw CreateAndLogValidationException(invalidPostException);
+            }
+        }
+
+        private PostValidationException CreateAndLogValidationException(InvalidPostException invalidPostException)
+        {
+            var postValidationException =
+                new PostValidationException(invalidPostException);
+
+            this.loggingBroker.LogError(postValidationException);
+
+            return postValidationException;
+        }
+    }
+}
