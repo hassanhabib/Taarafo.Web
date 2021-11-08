a91d72bd1509a5201a022aaeff87836d97993897
Stanislaw Nowak <microsoft@stas.email>
2021-11-08 23:58:38 +0100

    Add Blazor Button Base

---
+++ Taarafo.Portal.Web/Views/Bases/ButtonBase.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Taarafo.Portal.Web.Views.Bases
+{
+    public partial class ButtonBase : ComponentBase
+    {
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public Action OnClick { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        public void Click() => OnClick.Invoke();
+
+        public void Disable()
+        {
+            this.IsDisabled = true;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Enable()
+        {
+            this.IsDisabled = false;
+            InvokeAsync(StateHasChanged);
+        }
+    }
+}
