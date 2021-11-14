920c0bcf49851a564f23a1643d003ec5e2bb8f87
Stanislaw Nowak <microsoft@stas.email>
2021-11-14 11:47:28 +0100

    Add Syncfusion Blazor Buttons

--- Taarafo.Portal.Web/Startup.cs
+++ Taarafo.Portal.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Syncfusion.Blazor;
 using Taarafo.Portal.Web.Brokers.API;
 using Taarafo.Portal.Web.Brokers.Loggings;
 
@@ -24,6 +25,7 @@ namespace Taarafo.Portal.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSyncfusionBlazor();
             AddRootDirectory(services);
             services.AddLogging();
             services.AddHttpClient();
