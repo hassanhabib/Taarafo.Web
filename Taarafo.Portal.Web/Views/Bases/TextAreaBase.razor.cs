23da3a33f522ce57de01f98e3d25401cd2480f8e
Stanislaw Nowak <microsoft@stas.email>
2021-11-14 11:33:39 +0100

    Add Text Area Base

---
+++ Taarafo.Portal.Web/Views/Bases/TextAreaBase.razor.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Taarafo.Portal.Web.Views.Bases
+{
+    public partial class TextAreaBase : ComponentBase
+    {
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public string Placeholder { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        [Parameter]
+        public bool IsDisabled { get; set; }
+
+        public bool IsEnabled => IsDisabled is false;
+
+        private Task OnValueChanged(ChangeEventArgs changeEventArgs)
+        {
+            this.Value = changeEventArgs.Value.ToString();
+
+            return ValueChanged.InvokeAsync(this.Value);
+        }
+
+    }
+}
