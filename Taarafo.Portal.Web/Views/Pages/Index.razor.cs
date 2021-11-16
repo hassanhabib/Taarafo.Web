84adc0613dfae01114e6daa71825e8295e58f0a2
Stanislaw Nowak <microsoft@stas.email>
2021-11-16 19:30:33 +0100

    Working Close Button Example

---
+++ Taarafo.Portal.Web/Views/Pages/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using Taarafo.Portal.Web.Views.Bases;
+
+namespace Taarafo.Portal.Web.Views.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public DialogBase Dialog { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Dialog.Show();
+        }
+        
+        public void CloseDialog()
+        {
+            Dialog.Hide();
+        }
+    }
+}
