6c0abb7f2363f018f1590679c1d1c591e694d0f4
Hassan Habib <hassanhabib@live.com>
2021-11-15 19:53:51 -0800

    ShouldThrowServiceExceptionOnRemoveIfServiceErrorOccursAndLogItAsync -> FAIL

--- Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RemoveById.cs
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RemoveById.cs
@@ -266,5 +266,43 @@ namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Posts
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid somePostId = Guid.NewGuid();
+            var serviceException = new Exception();
+            
+            var failedPostServiceException = 
+                new FailedPostServiceException(serviceException);
+
+            var expectedPostServiceException =
+                new PostServiceException(failedPostServiceException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.DeletePostByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Post> removePostByIdTask =
+                this.postService.RemovePostByIdAsync(somePostId);
+
+            // then
+            await Assert.ThrowsAsync<PostServiceException>(() =>
+                removePostByIdTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.DeletePostByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostServiceException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ Taarafo.Portal.Web/Models/Posts/Exceptions/PostServiceException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace Taarafo.Portal.Web.Models.Posts.Exceptions
+{
+    public class PostServiceException : Xeption
+    {
+        public PostServiceException(Xeption innerException)
+            : base(message: "Post service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
