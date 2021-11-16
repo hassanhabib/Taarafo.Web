91dff11053af6330083f1c9e7f1c9a8e041dc7a8
Stanislaw Nowak <microsoft@stas.email>
2021-11-16 19:30:06 +0100

    Add Dialog Base

---
+++ Taarafo.Portal.Web/Views/Bases/DialogBase.razor.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Taarafo.Portal.Web.Views.Bases
+{
+    public partial class DialogBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+        [Parameter]
+        public Action OnClick { get; set; }
+
+        [Parameter]
+        public string Width { get; set; }
+
+        public bool IsVisible { get; set; }
+
+        public void Show()
+        {
+            IsVisible = true;
+            InvokeAsync(StateHasChanged);
+        } 
+        public void Hide()
+        {
+            IsVisible = false;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Click() => OnClick?.Invoke();
+    }
+}
