70d89b7fc9de0f9dac36ec75b5d11d5b8fecb7c9
Ricky <rcruzfreelance@outlook.com>
2021-11-15 23:46:35 -0400

    ShouldThrowPostDependencyExceptionOnRemoveIfDependencyErrorOccursAndLogItAsync -> FAIL

--- Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RemoveById.cs
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RemoveById.cs
@@ -221,5 +221,50 @@ namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Posts
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowPostDependencyExceptionOnRemoveIfDependencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid somePostId = Guid.NewGuid();
+            string someMessage = GetRandomMessage();
+            var httpResponseMessage = new HttpResponseMessage();
+
+            var httpResponseException =
+                new HttpResponseException(
+                        httpResponseMessage,
+                        someMessage);
+
+            var failedPostDependencyException =
+                new FailedPostDependencyException(httpResponseException);
+
+            var expectedPostDependencyException =
+                new PostDependencyException(failedPostDependencyException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.DeletePostByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(httpResponseException);
+
+
+            // when
+            ValueTask<Post> removePostByIdTask =
+                this.postService.RemovePostByIdAsync(somePostId);
+
+            // then
+            await Assert.ThrowsAsync<PostDependencyException>(() =>
+                removePostByIdTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.DeletePostByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostDependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
