0921d6d95c7760b60d16846f29d9f5402408220a
Stanislaw Nowak <microsoft@stas.email>
2021-11-18 00:27:19 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfCriticalDependencyExceptionOccursAndLogItAsync => FAIL

---
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Portal.Web.Models.Posts;
+using Taarafo.Portal.Web.Models.Posts.Exceptions;
+using Xunit;
+
+namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyExceptions))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfCriticalDependencyExceptionOccursAndLogItAsync(
+            Exception criticalDependencyException)
+        {
+            var failedPostDependencyException =
+                new FailedPostDependencyException(criticalDependencyException);
+
+            var expectedPostDependencyException =
+                new PostDependencyException(
+                    failedPostDependencyException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.GetAllPostsAsync())
+                    .ThrowsAsync(criticalDependencyException);
+
+            ValueTask<List<Post>> retrieveAllPostsTask =
+                this.postService.RetrieveAllPostsAsync();
+
+            await Assert.ThrowsAsync<PostDependencyException>(() =>
+               retrieveAllPostsTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.GetAllPostsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedPostDependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
