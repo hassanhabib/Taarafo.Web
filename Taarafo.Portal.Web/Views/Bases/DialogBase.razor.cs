f591833909fda6395dfbb2cfe684f9228278e286
Stanislaw Nowak <microsoft@stas.email>
2021-11-17 17:49:37 +0100

    BASE: DialogBase

---
+++ Taarafo.Portal.Web/Views/Bases/DialogBase.razor.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Taarafo.Portal.Web.Views.Bases
+{
+    public partial class DialogBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public bool IsVisible { get; set; }
+
+        [Parameter]
+        public string Width { get; set; }
+
+        [Parameter]
+        public RenderFragment Content { get; set; }
+
+        [Parameter]
+        public string ButtonTitle { get; set; }
+
+        [Parameter]
+        public Action OnClick { get; set; }
+
+        public void Click() => OnClick?.Invoke();
+
+        public void Show()
+        {
+            IsVisible = true;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Hide()
+        {
+            IsVisible = false;
+            InvokeAsync(StateHasChanged);
+        }
+    }
+}
