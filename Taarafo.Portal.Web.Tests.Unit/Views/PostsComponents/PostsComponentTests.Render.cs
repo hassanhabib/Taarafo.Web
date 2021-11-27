7abae3d1ded9e949aab75e9c175beea8f03be2e2
Stanislaw Nowak <microsoft@stas.email>
2021-11-27 02:37:07 +0100

    ShouldRenderPosts => FAIL

--- Taarafo.Portal.Web.Tests.Unit/Views/PostsComponents/PostsComponentTests.Render.cs
+++ Taarafo.Portal.Web.Tests.Unit/Views/PostsComponents/PostsComponentTests.Render.cs
@@ -3,8 +3,11 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Collections.Generic;
 using Bunit;
 using FluentAssertions;
+using Moq;
+using Taarafo.Portal.Web.Models.PostViews;
 using Taarafo.Portal.Web.Models.Views.Components.PostsComponents;
 using Taarafo.Portal.Web.Views.Components.PostsComponents;
 using Xunit;
@@ -31,6 +34,56 @@ namespace Taarafo.Portal.Web.Tests.Unit.Views.PostsComponents
             initialPostsComponent.Grid.Should().BeNull();
             initialPostsComponent.ErrorMessage.Should().BeNull();
             initialPostsComponent.ErrorLabel.Should().BeNull();
+        }
+
+        [Fact]
+        public void ShouldRenderPosts()
+        {
+            // given
+            PostsComponentState expectedState =
+                PostsComponentState.Content;
+
+            List<PostView> randomPostViews =
+                CreateRandomPostViews();
+
+            List<PostView> retrievedPostViews =
+                randomPostViews;
+
+            List<PostView> expectedPostViews =
+                retrievedPostViews;
+
+            this.postViewServiceMock.Setup(service =>
+                service.RetrieveAllPostViewsAsync())
+                    .ReturnsAsync(retrievedPostViews);
+
+            // when
+            this.renderedPostsComponent =
+                RenderComponent<PostsComponent>();
+
+            // then
+            this.renderedPostsComponent.Instance.State.Should()
+                .Be(expectedState);
+
+            this.renderedPostsComponent.Instance.PostViews.Should()
+                .BeEquivalentTo(expectedPostViews);
+
+            this.renderedPostsComponent.Instance.Grid.Should()
+                .NotBeNull();
+
+            this.renderedPostsComponent.Instance.Grid.DataSource.Should()
+                .BeEquivalentTo(expectedPostViews);
+
+            this.renderedPostsComponent.Instance.ErrorMessage.Should()
+                .BeNull();
+
+            this.renderedPostsComponent.Instance.ErrorLabel.Should()
+                .BeNull();
+
+            this.postViewServiceMock.Verify(service =>
+                service.RetrieveAllPostViewsAsync(),
+                    Times.Once);
+
+            this.postViewServiceMock.VerifyNoOtherCalls();
         }
     }
 }
--- Taarafo.Portal.Web.Tests.Unit/Views/PostsComponents/PostsComponentTests.cs
+++ Taarafo.Portal.Web.Tests.Unit/Views/PostsComponents/PostsComponentTests.cs
@@ -6,13 +6,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Syncfusion.Blazor;
+using Taarafo.Portal.Web.Models.PostViews;
+using Taarafo.Portal.Web.Services.PostViews;
+using Taarafo.Portal.Web.Views.Components.PostsComponents;
+using Tynamix.ObjectFiller;
 
 namespace Taarafo.Portal.Web.Tests.Unit.Views.PostsComponents
 {
     public partial class PostsComponentTests : TestContext
     {
+        private readonly Mock<IPostViewService> postViewServiceMock;
+        private IRenderedComponent<PostsComponent> renderedPostsComponent;
+
+        public PostsComponentTests()
+        {
+            this.postViewServiceMock = new Mock<IPostViewService>();
+            this.Services.AddTransient(service => this.postViewServiceMock.Object);
+            this.Services.AddSyncfusionBlazor();
+            this.Services.AddOptions();
+            this.JSInterop.Mode = JSRuntimeMode.Loose;
+        }
+
+        private static string GetRandomMessage() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private static List<PostView> CreateRandomPostViews() =>
+            CreatePostViewFiller().Create(count: GetRandomNumber()).ToList();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 10).GetValue();
+
+        private static Filler<PostView> CreatePostViewFiller() 
+        {
+            var filler = new Filler<PostView>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+            return filler;
+        }
     }
 }
