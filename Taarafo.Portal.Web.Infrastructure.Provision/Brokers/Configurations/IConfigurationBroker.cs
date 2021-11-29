ab34d0e29a90607e4a661fa493678b783112d055
Christo du Toit <christo@dutoit.co.uk>
2021-11-29 17:28:08 +0000

    BROKERS: Implement Configuration Brokers

---
+++ Taarafo.Portal.Web.Infrastructure.Provision/Brokers/Configurations/IConfigurationBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Taarafo.Portal.Web.Infrastructure.Provision.Models.Configurations;
+
+namespace Taarafo.Portal.Web.Infrastructure.Provision.Brokers.Configurations
+{
+    public interface IConfigurationBroker
+    {
+        CloudManagementConfiguration GetConfigurations();
+    }
+}
