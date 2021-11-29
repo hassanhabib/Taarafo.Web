672e5cec6b937220e93a7747c6fd07758b131c8a
Christo du Toit <christo@dutoit.co.uk>
2021-11-29 17:31:08 +0000

    FOUNDATIONS: Implement Provisioning Resource Group

---
+++ Taarafo.Portal.Web.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Taarafo.Portal.Web.Infrastructure.Provision.Brokers.Clouds;
+using Taarafo.Portal.Web.Infrastructure.Provision.Brokers.Loggings;
+
+namespace Taarafo.Portal.Web.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService()
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public async ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            string resourceGroupName = $"{projectName}-RESOURCES-{environment}".ToUpper();
+            this.loggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}...");
+
+            IResourceGroup resourceGroup =
+                await this.cloudBroker.CreateResourceGroupAsync(
+                    resourceGroupName);
+
+            this.loggingBroker.LogActivity(message: $"{resourceGroupName} Provisioned.");
+
+            return resourceGroup;
+        }
+    }
+}
