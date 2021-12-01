5217c44a12dcd7b0c731562751761e178c78fba4
Christo du Toit <christo@dutoit.co.uk>
2021-12-01 16:52:43 +0000

    BROKERS: Post Comment

---
+++ Taarafo.Portal.Web/Brokers/API/IApiBroker.Comments.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Taarafo.Portal.Web.Models.Comments;
+
+namespace Taarafo.Portal.Web.Brokers.API
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<Comment> PostCommentAsync(Comment comment);
+    }
+}
