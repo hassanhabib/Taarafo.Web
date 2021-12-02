0beaedde0984615b0c5ce218440870c889a92598
Christo du Toit <christo@dutoit.co.uk>
2021-12-02 19:11:56 +0000

    BROKERS: Post Comment

---
+++ Taarafo.Portal.Web/Brokers/API/ApiBroker.Comments.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Taarafo.Portal.Web.Models.Comments;
+
+namespace Taarafo.Portal.Web.Brokers.API
+{
+    public partial class ApiBroker
+    {
+        private const string commentsRelativeUrl = "api/comments";
+
+        public async ValueTask<Comment> PostCommentAsync(Comment comment) =>
+                    await this.PostAsync(commentsRelativeUrl, comment);
+    }
+}
