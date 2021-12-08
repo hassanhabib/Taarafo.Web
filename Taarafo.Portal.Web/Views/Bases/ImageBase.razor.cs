beaa09a3a950afb91c0db43b8d301d0f8d59fd39
Stanislaw Nowak <microsoft@stas.email>
2021-12-08 18:49:03 +0100

    Create ImageBase component

---
+++ Taarafo.Portal.Web/Views/Bases/ImageBase.razor.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace Taarafo.Portal.Web.Views.Bases
+{
+    public partial class ImageBase : ComponentBase
+    {
+        [Parameter]
+        public string Url { get; set; }
+
+        [Parameter]
+        public string Width { get; set; }
+    }
+}
