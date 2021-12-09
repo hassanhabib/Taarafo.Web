adc9256ea0aa72081209e6c3fbaa712c7fb78033
Stanislaw Nowak <microsoft@stas.email>
2021-12-09 12:38:30 +0100

    ShouldThrowValidationExceptionOnAddIfPostIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.Add.cs
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.Add.cs
@@ -46,5 +46,65 @@ namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Posts
             this.apiBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async void ShouldThrowValidationExceptionOnAddIfPostIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            var invalidPost = new Post
+            {
+                Content = invalidText,
+                Author = invalidText
+            };
+
+            var invalidPostException = new InvalidPostException();
+
+            invalidPostException.AddData(
+                key: nameof(Post.Id),
+                values: "Id is required");
+
+            invalidPostException.AddData(
+                key: nameof(Post.Content),
+                values: "Text is required");
+
+            invalidPostException.AddData(
+                key: nameof(Post.CreatedDate),
+                values: "Date is required");
+
+            invalidPostException.AddData(
+                key: nameof(Post.UpdatedDate),
+                values: "Date is required");
+
+            invalidPostException.AddData(
+                key: nameof(Post.Author),
+                values: "Text is required");
+
+            var expectedPostValidationException =
+                new PostValidationException(invalidPostException);
+
+            // when
+            ValueTask<Post> addPostTask =
+                this.postService.AddPostAsync(invalidPost);
+
+            // then
+            await Assert.ThrowsAsync<PostValidationException>(() =>
+                addPostTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostValidationException))),
+                        Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostPostAsync(It.IsAny<Post>()),
+                    Times.Never);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
