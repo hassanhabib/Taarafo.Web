a0978c46aacc981d3fff510dabc8beda78b05b87
Stanislaw Nowak <microsoft@stas.email>
2021-12-13 01:45:25 +0100

    ShouldThrowValidationExceptionOnAddIfPostViewIsInvalidAndLogItAsync -> FAIL

--- Taarafo.Portal.Web.Tests.Unit/Services/Views/PostViews/PostViewServiceTests.Validations.Add.cs
+++ Taarafo.Portal.Web.Tests.Unit/Services/Views/PostViews/PostViewServiceTests.Validations.Add.cs
@@ -55,5 +55,60 @@ namespace Taarafo.Portal.Web.Tests.Unit.Services.Views.PostViews
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.postServiceMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfPostViewIsInvalidAndLogItAsync(
+            string invalidText)
+        {
+            // given
+            PostView invalidPostView = new PostView
+            {
+                Content = invalidText,
+            };
+
+            var invalidPostViewException =
+                new InvalidPostViewException();
+
+            invalidPostViewException.AddData(
+                key: nameof(Post.Content),
+                values: "Text is required");
+
+            var expectedPostViewValidationException =
+                new PostViewValidationException(invalidPostViewException);
+
+            // when
+            ValueTask<PostView> addPostViewTask =
+                this.postViewService.AddPostViewAsync(
+                    invalidPostView);
+
+            // then
+            await Assert.ThrowsAsync<PostViewValidationException>(() =>
+                addPostViewTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostViewValidationException))),
+                        Times.Once);
+
+            this.authorServiceMock.Verify(service =>
+                service.GetCurrentlyLoggedInAuthor(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Never);
+
+            this.postServiceMock.Verify(service =>
+                service.AddPostAsync(It.IsAny<Post>()),
+                    Times.Never);
+
+            this.postServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.authorServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
