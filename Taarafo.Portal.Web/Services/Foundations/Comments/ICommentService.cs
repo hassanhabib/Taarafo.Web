5db5cebc62bdc81cda17fbed95c8af16abd44494
Christo du Toit <christo@dutoit.co.uk>
2021-12-29 18:20:02 +0000

    ShouldRetrieveCommentByIdAsync -> FAIL

--- Taarafo.Portal.Web/Services/Foundations/Comments/ICommentService.cs
+++ Taarafo.Portal.Web/Services/Foundations/Comments/ICommentService.cs
@@ -3,6 +3,7 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Taarafo.Portal.Web.Models.Comments;
@@ -13,5 +14,6 @@ namespace Taarafo.Portal.Web.Services.Foundations.Comments
     {
         ValueTask<Comment> AddCommentAsync(Comment comment);
         ValueTask<List<Comment>> RetrieveAllCommentsAsync();
+        ValueTask<Comment> RetrieveCommentByIdAsync(Guid commentId);
     }
 }
