b3372ba69a0ba4fa674f65543f3a860d34f05970
Christo du Toit <christo@dutoit.co.uk>
2021-12-29 19:24:45 +0000

    ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync -> PASS

---
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Portal.Web.Models.Comments;
+using Taarafo.Portal.Web.Models.Comments.Exceptions;
+using Xunit;
+
+namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyExceptions))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfCriticalErrorOccursAndLogItAsync(
+            Exception criticalDependencyException)
+        {
+            // given
+            Comment someComment = CreateRandomComment();
+
+            var failedCommentDependencyException =
+                new FailedCommentDependencyException(criticalDependencyException);
+
+            var expectedCommentDependencyException =
+                new CommentDependencyException(failedCommentDependencyException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PutCommentAsync(It.IsAny<Comment>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<Comment> modifyCommentTask =
+                this.commentService.ModifyCommentAsync(someComment);
+
+            // then
+            await Assert.ThrowsAsync<CommentDependencyException>(() =>
+               modifyCommentTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PutCommentAsync(It.IsAny<Comment>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedCommentDependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
