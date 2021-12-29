6d7a565bf985f27f4c2d9429a201e3c7ee510173
Christo du Toit <christo@dutoit.co.uk>
2021-12-29 17:21:33 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveIfCriticalErrorOccursAndLogItAsync -> PASS

---
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Portal.Web.Models.Comments;
+using Taarafo.Portal.Web.Models.Comments.Exceptions;
+using Xunit;
+
+namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(CriticalDependencyExceptions))]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveIfCriticalErrorOccursAndLogItAsync(
+            Exception criticalDependencyException)
+        {
+            // given
+            Guid someCommentId = Guid.NewGuid();
+
+            var failedCommentDependencyException =
+                new FailedCommentDependencyException(criticalDependencyException);
+
+            var expectedCommentDependencyException =
+                new CommentDependencyException(failedCommentDependencyException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.GetCommentByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(criticalDependencyException);
+
+            // when
+            ValueTask<Comment> retrieveCommentByIdTask =
+                this.commentService.RetrieveCommentByIdAsync(someCommentId);
+
+            // then
+            await Assert.ThrowsAsync<CommentDependencyException>(() =>
+               retrieveCommentByIdTask.AsTask());
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.GetCommentByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedCommentDependencyException))),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
