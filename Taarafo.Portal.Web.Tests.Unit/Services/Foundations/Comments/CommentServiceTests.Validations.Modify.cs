c4d9574ce50a2865478ced8c755ff49743280bc1
Christo du Toit <christo@dutoit.co.uk>
2021-12-29 19:12:05 +0000

    ShouldThrowValidationExceptionOnModifyWhenCommentIsNullAndLogItAsync -> FAIL

---
+++ Taarafo.Portal.Web.Tests.Unit/Services/Foundations/Comments/CommentServiceTests.Validations.Modify.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using Taarafo.Portal.Web.Models.Comments;
+using Taarafo.Portal.Web.Models.Comments.Exceptions;
+using Xunit;
+
+namespace Taarafo.Portal.Web.Tests.Unit.Services.Foundations.Comments
+{
+    public partial class CommentServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyWhenCommentIsNullAndLogItAsync()
+        {
+            // given
+            Comment nullComment = null;
+            var nullCommentException = new NullCommentException();
+
+            var expectedCommentValidationException =
+                new CommentValidationException(nullCommentException);
+
+            // when
+            ValueTask<Comment> modifyCommentTask =
+                this.commentService.ModifyCommentAsync(nullComment);
+
+            // then
+            await Assert.ThrowsAsync<CommentValidationException>(() =>
+                modifyCommentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCommentValidationException))),
+                    Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PutCommentAsync(It.IsAny<Comment>()),
+                    Times.Never);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
