e266b39d70c94b85cded15c6e1d71f06d77296e6
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-01-26 06:50:55 -0800

    ShouldAddPostViewAsync -> FAIL

---
+++ Taarafo.Portal.Web.Tests.Unit/Services/Views/CommentViews/CommentViewServiceTest.Logic.Add.cs
@@ -0,0 +1,77 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Taarafo.Portal.Web.Models.Comments;
+using Taarafo.Portal.Web.Models.CommentViews;
+using Xunit;
+
+namespace Taarafo.Portal.Web.Tests.Unit.Services.Views.CommentViews
+{
+    public partial class CommentViewServiceTest
+    {
+        [Fact] 
+        public async Task ShouldAddCommentViewAsync()
+        {
+            // given 
+            DateTimeOffset randomDateTime = GetRandomDate();
+            Guid randomPostId = GetRandomGuid();
+
+            dynamic randomCommentViewProperties =
+                CreateRandomCommentViewProperties(
+                    auditDates: randomDateTime,
+                    auditPostId: randomPostId);
+
+            var randomCommentView = new CommentView
+            {
+                Id = randomCommentViewProperties.Id,
+                Content = randomCommentViewProperties.Content,
+                CreatedDate = randomCommentViewProperties.CreatedDate,
+                UpdatedDate = randomCommentViewProperties.UpdatedDate, 
+                PostId = randomCommentViewProperties.PostId
+            };
+
+            CommentView inputCommentView = randomCommentView;
+            CommentView expectedCommentView = inputCommentView;
+
+            var randomComment = new Comment()
+            {
+                Id = randomCommentViewProperties.Id,
+                Content = randomCommentViewProperties.Content,
+                CreatedDate = randomCommentViewProperties.CreatedDate,
+                UpdatedDate = randomCommentViewProperties.UpdatedDate,
+                PostId = randomCommentViewProperties.PostId
+            };
+
+            Comment inputComment = randomComment;
+            Comment expectedComment = inputComment;
+
+            this.commentServiceMock.Setup(service =>
+                service.AddCommentAsync(It.Is(
+                    SameCommentAs(inputComment))))
+                    .ReturnsAsync(expectedComment);
+
+            // when 
+            CommentView actualCommentView =
+                await this.commentViewService
+                    .AddCommentViewAsync(inputCommentView);
+
+            // then
+            actualCommentView.Should().BeEquivalentTo(expectedCommentView);
+
+            this.commentServiceMock.Verify(service => 
+                service.AddCommentAsync(It.Is(
+                    SameCommentAs(inputComment))), 
+                Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.commentServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Taarafo.Portal.Web.Tests.Unit/Services/Views/CommentViews/CommentViewServiceTest.cs
@@ -0,0 +1,69 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq.Expressions;
+using KellermanSoftware.CompareNetObjects;
+using Moq;
+using Taarafo.Portal.Web.Brokers.DateTimes;
+using Taarafo.Portal.Web.Brokers.Loggings;
+using Taarafo.Portal.Web.Models.Comments;
+using Taarafo.Portal.Web.Models.Posts;
+using Taarafo.Portal.Web.Services.Foundations.Comments;
+using Taarafo.Portal.Web.Services.Views.CommentViews;
+using Tynamix.ObjectFiller;
+
+namespace Taarafo.Portal.Web.Tests.Unit.Services.Views.CommentViews
+{
+    public partial class CommentViewServiceTest
+    {
+        private readonly Mock<ICommentService> commentServiceMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly ICommentViewService commentViewService;
+        private readonly ICompareLogic compareLogic;
+
+        public CommentViewServiceTest()
+        {
+            this.commentServiceMock = new Mock<ICommentService>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            var compareConfig = new ComparisonConfig();
+            compareConfig.IgnoreProperty<Comment>(comment => comment.Id);
+            this.compareLogic = new CompareLogic(compareConfig);
+
+            this.commentViewService = new CommentViewService(
+                commentService: this.commentServiceMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        private static string GetRandomString() =>
+           new MnemonicString().GetValue();
+
+        private static DateTimeOffset GetRandomDate() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Guid GetRandomGuid() =>
+            Guid.NewGuid();
+
+        private dynamic CreateRandomCommentViewProperties(DateTimeOffset auditDates, Guid auditPostId)
+        {
+            return new
+            {
+                Id = Guid.NewGuid(),
+                Content = GetRandomString(),
+                CreatedDate = auditDates,
+                UpdatedDate = auditDates,
+                PostId = auditPostId
+            };
+        }
+
+        private Expression<Func<Comment, bool>> SameCommentAs(Comment expectedComment)
+        {
+            return actualComment => this.compareLogic.Compare(expectedComment, actualComment)
+                .AreEqual;
+        }
+    }
+}
