e7c6476cf01a2d7004be5b0262f2de39f1b7bbbd
evangelinedrink <Evangelinedrink@gmail.com>
2022-04-14 18:26:08 -0700

    BROKERS: Get All Groups

---
+++ Taarafo.Portal.Web/Models/Groups/GroupStatus.cs
@@ -0,0 +1,15 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+
+namespace Taarafo.Portal.Web.Models.Groups
+{
+    public enum GroupStatus
+    {
+        Active,
+        Inactive
+    }
+}
