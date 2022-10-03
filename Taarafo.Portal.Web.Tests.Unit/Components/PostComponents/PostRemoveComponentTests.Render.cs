787049f7a1a58ac65d081905e6c98577ecade953
Stanislaw Nowak <microsoft@stas.email>
2022-10-03 23:11:43 +0200

    ShouldRemovePostAsync -> FAIL

--- Taarafo.Portal.Web.Tests.Unit/Components/PostComponents/PostRemoveComponentTests.Render.cs
+++ Taarafo.Portal.Web.Tests.Unit/Components/PostComponents/PostRemoveComponentTests.Render.cs
@@ -3,11 +3,12 @@
 // FREE TO USE TO CONNECT THE WORLD
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Bunit;
 using FluentAssertions;
+using Moq;
 using Taarafo.Portal.Web.Models.PostViews;
 using Taarafo.Portal.Web.Models.Views.Components.PostComponents;
-using Taarafo.Portal.Web.Models.Views.Components.PostDialogs;
 using Taarafo.Portal.Web.Views.Components.PostComponents;
 using Taarafo.Portal.Web.Views.Components.PostDialogs;
 using Xunit;
@@ -34,7 +35,7 @@ namespace Taarafo.Portal.Web.Tests.Unit.Components.PostComponents
         }
 
         [Fact]
-        public void ShouldRenderComponent() 
+        public void ShouldRenderComponent()
         {
             // given
             PostRemoveComponentState expectedState =
@@ -62,6 +63,41 @@ namespace Taarafo.Portal.Web.Tests.Unit.Components.PostComponents
             this.postRemoveRenderedComponent.Instance.Button.Label.Should().Be(expectedLabel);
             this.postRemoveRenderedComponent.Instance.Button.IsDisabled.Should().BeFalse();
             this.postRemoveRenderedComponent.Instance.Button.OnClick.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ShouldRemovePostAsync()
+        {
+            // given
+            PostView randomPostView = CreateRandomPostView();
+            PostView inputPostView = randomPostView;
+            PostView expectedPostView = inputPostView;
+
+            ComponentParameter inputComponentParameter =
+                ComponentParameter.CreateParameter(
+                    name: nameof(PostView),
+                    value: inputPostView);
+
+            // when
+            this.postRemoveRenderedComponent =
+                RenderComponent<PostRemoveComponent>(inputComponentParameter);
+
+            this.postRemoveRenderedComponent.Instance.Button
+                .Click();
+
+            // then
+            this.postRemoveRenderedComponent.Instance.Button.IsDisabled
+                .Should().BeTrue();
+
+            this.postRemoveRenderedComponent.Instance.PostView
+                .Should().BeEquivalentTo(expectedPostView);
+
+            this.postViewServiceMock.Verify(service =>
+                service.RemovePostViewByIdAsync(
+                    this.postRemoveRenderedComponent.Instance.PostView.Id),
+                        Times.Once);
+
+            this.postViewServiceMock.VerifyNoOtherCalls();
         }
     }
 }
